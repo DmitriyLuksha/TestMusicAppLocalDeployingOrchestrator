cc63dbd7a404d426edbc05833d0e7ee01b07c367
nanpx <nanpxmail@gmail.com>
2020-02-29 18:47:20 +0300

    Add server publishing

--- Publish-AllProjects.ps1
+++ Publish-AllProjects.ps1
@@ -2,3 +2,6 @@ $ErrorActionPreference = "Stop";
 
 Write-Host "Publishing database";
 & "$PSScriptRoot\..\TestMusicAppDb\Scripts\Publish-Database.ps1";
+
+Write-Host "Publishing server";
+& "$PSScriptRoot\..\TestMusicAppServer\Scripts\Publish-Server.ps1";
